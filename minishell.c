784621afae42c39f38f0c364c81ceb07812db1e9
Hari <149360252+Harimino123@users.noreply.github.com>
2024-09-25 10:00:20 +0300

    Add files via upload

---
+++ minishell.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrasolof <hrasolof@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 09:39:06 by hrasolof          #+#    #+#             */
+/*   Updated: 2024/09/25 09:59:06 by hrasolof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*#############THIS IS THE BEGINNIG WHERE WE WRITE PROMPT#######################*/
+// int main(void)
+// {
+//     while (1)
+//     {
+//         char *rl;
+//         rl = readline("Prompt > ");
+//         printf("%s\n", rl);
+//     }
+//     return (0); 
+// }
+
+
+// /*#############WORK AS PWD IN BASH#######################*/
+// int main(void)
+// {
+//     char *pwd;
+    
+//     pwd = getcwd(NULL, 0);
+//     printf("pwd: %s\n", pwd);
+//     return (0);
+// }
+
+/*#############CHANGE CURRENTLY WORKING DIRECTORY#######################*/
+// int main(void)
+// {
+//     char *pwd;
+    
+//     pwd = getcwd(NULL, 0);
+//     printf("pwd before chdir: %s\n", pwd);
+//     chdir("/home/hrasolof");
+//     pwd = getcwd(NULL, 0);
+//     printf("pwd after chdir: %s\n", pwd);
+//     return (0);
+// }
+
---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrasolof <hrasolof@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 09:39:03 by hrasolof          #+#    #+#             */
+/*   Updated: 2024/09/25 09:40:43 by hrasolof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
