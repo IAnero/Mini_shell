aa8b5edfe409849996698f5c903f67210b74a4c8
Hari <hari.rasolofoniaina@gmail.com>
2024-09-28 11:05:11 +0300

    Cleaning Makefile

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: hrasolof <hrasolof@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 09:39:06 by hrasolof          #+#    #+#             */
-/*   Updated: 2024/09/27 11:37:44 by hrasolof         ###   ########.fr       */
+/*   Updated: 2024/09/27 18:43:28 by hrasolof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ int main(void)
         if (*input)
             add_history(input); 
         args = ft_toksplit(input);
-        
         // if (built_in_command(args) == 0)
         //     execute_commad(args);
         built_in_command(args);
